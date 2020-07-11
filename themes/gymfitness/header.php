a96479977c63cd2c32f2b80cbf3bc11c915b5762
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-10 18:13:27 -0700

    adding header and footer files

---
+++ themes/gymfitness/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GymFitness</title>
+</head>
+
+<body>
+    <header class="site-header">
+        <div class="container header-grid">
+            <div class="navigation-bar">
+                <div class="logo">
+                    <a href="<?php echo home_url() ?>">
+                        <img src="<?php echo get_template_directory_uri() . "/img/logo.svg" ?>" alt="GymFitness Logo" />
+                    </a>
+
+                </div>
+            </div>
+        </div>
+
+    </header>
--- themes/gymfitness/page.php
+++ themes/gymfitness/page.php
@@ -1,4 +1,6 @@
-<?php while(have_posts()): the_post();?>
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
-<?php endwhile;?>
+<?php get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+<?php endwhile; ?>
