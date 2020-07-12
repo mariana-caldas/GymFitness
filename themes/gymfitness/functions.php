3b58ba50605087e39c9b787c77813ee5f33bfdbc
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-11 17:14:57 -0700

    adding Google Fonts and the main stylesheet

--- themes/gymfitness/functions.php
+++ themes/gymfitness/functions.php
@@ -13,6 +13,10 @@ add_action('init', 'gymfitness_menus' );
 function gymfitness_scripts(){
     //Normalize CSS.
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+    //Google Fonts
+    wp_enqueue_style('googleFonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@400;700;900&family=Staatliches&display=swap', array(), '1.0.0');
+    //Main Stylesheet
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFonts'), '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
 
