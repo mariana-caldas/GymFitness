a5632a0fd49f91ea464f341e6ab54e69563fc965
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-14 15:33:14 -0700

    adding featured image functions to the pages

--- themes/gymfitness/functions.php
+++ themes/gymfitness/functions.php
@@ -26,4 +26,11 @@ function gymfitness_scripts(){
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
 
+//Enables feature images and other stuff
+function gymfitness_setup(){
+    //Add featured image
+    add_theme_support( 'post-thumbnails');
+}
+add_action('after_setup_theme', 'gymfitness_setup');
+
 ?>
--- themes/gymfitness/page.php
+++ themes/gymfitness/page.php
@@ -1,8 +1,18 @@
 <?php get_header(); ?>
-    <div class="container">
+    <main class="container page">
         <?php while (have_posts()) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+        <?php 
+          if(has_post_thumbnail()):
+             the_post_thumbnail();
+          else:
+            echo "<p>No featured image uploaded to this page</p>";
+          endif;
+        
+        ?>
+        <div class="text-center">
+            <?php the_content(); ?>
+        </div>
         <?php endwhile; ?>
-    </div>
+    </main>
 <?php get_footer(); ?>
