e27e69217e1b68a6a12ab0a8eb7a4885b4fc71c8
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-16 14:40:09 -0700

    correcting the text domain on the custom post type function

--- plugins/gymfitness_post_types/gymfitness_post_types.php
+++ plugins/gymfitness_post_types/gymfitness_post_types.php
@@ -13,40 +13,39 @@
  if(!defined('ABSPATH')) die();
  
 // Register new Custom Post Type
-function gymfitness_class_post_type() {
-
+function gymfitness_class_post_type(){
 	$labels = array(
-		'name'                  => _x( 'Classes', 'Post Type General Name', 'gymfitness' ),
-		'singular_name'         => _x( 'Class', 'Post Type Singular Name', 'gymfitness' ),
-		'menu_name'             => __( 'Classes', 'gymfitness' ),
-		'name_admin_bar'        => __( 'Classes', 'gymfitness' ),
-		'archives'              => __( 'Archive', 'gymfitness' ),
-		'attributes'            => __( 'Attributes', 'gymfitness' ),
-		'parent_item_colon'     => __( 'Parent Class', 'gymfitness' ),
-		'all_items'             => __( 'All Classes', 'gymfitness' ),
-		'add_new_item'          => __( 'Add Class', 'gymfitness' ),
-		'add_new'               => __( 'Add Class', 'gymfitness' ),
-		'new_item'              => __( 'New Class', 'gymfitness' ),
-		'edit_item'             => __( 'Edit Class', 'gymfitness' ),
-		'update_item'           => __( 'Update Class', 'gymfitness' ),
-		'view_item'             => __( 'View Class', 'gymfitness' ),
-		'view_items'            => __( 'View Class', 'gymfitness' ),
-		'search_items'          => __( 'Search Class', 'gymfitness' ),
-		'not_found'             => __( 'Not found', 'gymfitness' ),
-		'not_found_in_trash'    => __( 'Not found in trash', 'gymfitness' ),
-		'featured_image'        => __( 'Featured Image', 'gymfitness' ),
-		'set_featured_image'    => __( 'Save Featured Image', 'gymfitness' ),
-		'remove_featured_image' => __( 'Remove Featured Image', 'gymfitness' ),
-		'use_featured_image'    => __( 'Use as Featured Image', 'gymfitness' ),
-		'insert_into_item'      => __( 'Insert in Class', 'gymfitness' ),
-		'uploaded_to_this_item' => __( 'Add in Class', 'gymfitness' ),
-		'items_list'            => __( 'Classes List', 'gymfitness' ),
-		'items_list_navigation' => __( 'Navigate to Classes', 'gymfitness' ),
-		'filter_items_list'     => __( 'Filter Classes', 'gymfitness' ),
+		'name'                  => _x( 'Classes', 'Post Type General Name', 'gymfitness-post-types' ),
+		'singular_name'         => _x( 'Class', 'Post Type Singular Name', 'gymfitness-post-types' ),
+		'menu_name'             => __( 'Classes', 'gymfitness-post-types' ),
+		'name_admin_bar'        => __( 'Classes', 'gymfitness-post-types' ),
+		'archives'              => __( 'Archive', 'gymfitness-post-types' ),
+		'attributes'            => __( 'Attributes', 'gymfitness-post-types' ),
+		'parent_item_colon'     => __( 'Parent Class', 'gymfitness-post-types' ),
+		'all_items'             => __( 'All Classes', 'gymfitness-post-types' ),
+		'add_new_item'          => __( 'Add Class', 'gymfitness-post-types' ),
+		'add_new'               => __( 'Add Class', 'gymfitness-post-types' ),
+		'new_item'              => __( 'New Class', 'gymfitness-post-types' ),
+		'edit_item'             => __( 'Edit Class', 'gymfitness-post-types' ),
+		'update_item'           => __( 'Update Class', 'gymfitness-post-types' ),
+		'view_item'             => __( 'View Class', 'gymfitness-post-types' ),
+		'view_items'            => __( 'View Class', 'gymfitness-post-types' ),
+		'search_items'          => __( 'Search Class', 'gymfitness-post-types' ),
+		'not_found'             => __( 'Not found', 'gymfitness-post-types' ),
+		'not_found_in_trash'    => __( 'Not found in trash', 'gymfitness-post-types' ),
+		'featured_image'        => __( 'Featured Image', 'gymfitness-post-types' ),
+		'set_featured_image'    => __( 'Save Featured Image', 'gymfitness-post-types' ),
+		'remove_featured_image' => __( 'Remove Featured Image', 'gymfitness-post-types' ),
+		'use_featured_image'    => __( 'Use as Featured Image', 'gymfitness-post-types' ),
+		'insert_into_item'      => __( 'Insert in Class', 'gymfitness-post-types' ),
+		'uploaded_to_this_item' => __( 'Add in Class', 'gymfitness-post-types' ),
+		'items_list'            => __( 'Classes List', 'gymfitness-post-types' ),
+		'items_list_navigation' => __( 'Navigate to Classes', 'gymfitness-post-types' ),
+		'filter_items_list'     => __( 'Filter Classes', 'gymfitness-post-types' ),
 	);
 	$args = array(
-		'label'                 => __( 'Class', 'gymfitness' ),
-		'description'           => __( 'Classes for GymFitness Website', 'gymfitness' ),
+		'label'                 => __( 'Class', 'gymfitness-post-types' ),
+		'description'           => __( 'Classes for a Gym Fitness Website', 'gymfitness-post-types' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor', 'thumbnail' ),
 		'hierarchical'          => false,
@@ -63,7 +62,7 @@ function gymfitness_class_post_type() {
 		'publicly_queryable'    => true,
         'capability_type'       => 'page',
 	);
-	register_post_type( 'gymfitness_classes', $args );
+	register_post_type( 'gymfitness-classes', $args );
 
 }
 add_action( 'init', 'gymfitness_class_post_type', 0 );
