d53cf8a6479f5f34a9dbe05e98f24a6c8c480e23
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-15 22:38:31 -0700

    isolating the page loop that renders the content

--- themes/gymfitness/page-with-sidebar.php
+++ themes/gymfitness/page-with-sidebar.php
@@ -6,15 +6,7 @@ get_header();
 ?>
     <main class="container page with-sidebar">
         <div class="main-content">
-            <?php while (have_posts()) : the_post(); ?>
-            <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-            <?php 
-                if(has_post_thumbnail()):
-                    the_post_thumbnail('blog');
-                endif;
-            ?>
-            <?php the_content(); ?>
-            <?php endwhile; ?>
+            <?php get_template_part('template-parts/page', 'loop')?>
         </div>
         <?php get_sidebar(); ?>
     </main>
