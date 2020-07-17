6681d11eabb0f42575e2cf978fef41c80092cd8e
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-17 13:19:13 -0700

    adding all the content to the classes page

--- themes/gymfitness/inc/queries.php
+++ themes/gymfitness/inc/queries.php
@@ -9,9 +9,9 @@ function gymfitness_classes_list(){ ?>
             'orderby' => 'title', 
             'order' => 'ASC', 
         );   
-        $theClasses = new WP_Query($args);
+        $classes = new WP_Query($args);
 
-        while($theClasses->have_posts()): $theClasses->the_post();
+        while($classes->have_posts()): $classes->the_post();
     ?>
         <li class="gym-class card gradient">
             <?php the_post_thumbnail('mediumSize'); ?>
@@ -20,7 +20,13 @@ function gymfitness_classes_list(){ ?>
         <div class="card-content">
             <a href="<?php the_permalink(); ?>" title="Check the Class Description">
                 <h3><?php the_title(); ?></h3>
-            </a>    
+            </a>
+            <p><?php the_field('class_days'); ?></p>
+            <?php
+                $start_time = get_field('start_time');
+                $end_time = get_field('end_time');
+            ?>
+            <p><?php echo $start_time . " to " . $end_time ?></p>    
         </div>
     <?php endwhile; wp_reset_postdata(); ?>
     </ul>
