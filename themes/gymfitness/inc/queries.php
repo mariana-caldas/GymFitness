39a867390be13031f438998426dbb903a7292836
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-17 16:31:37 -0700

    styling page classes

--- themes/gymfitness/inc/queries.php
+++ themes/gymfitness/inc/queries.php
@@ -15,22 +15,19 @@ function gymfitness_classes_list(){ ?>
     ?>
         <li class="gym-class card gradient">
             <?php the_post_thumbnail('mediumSize'); ?>
-            
+                <div class="card-content">
+                <a href="<?php the_permalink(); ?>" title="Check the Class Description">
+                    <h3><?php the_title(); ?></h3>
+                </a>
+                <?php
+                    $start_time = get_field('start_time');
+                    $end_time = get_field('end_time');
+                ?>  
+                <p><?php echo the_field('class_days') . " - " . $start_time . " to " . $end_time ?></p>
+                </div>
         </li>
-        <div class="card-content">
-            <a href="<?php the_permalink(); ?>" title="Check the Class Description">
-                <h3><?php the_title(); ?></h3>
-            </a>
-            <p><?php the_field('class_days'); ?></p>
-            <?php
-                $start_time = get_field('start_time');
-                $end_time = get_field('end_time');
-            ?>
-            <p><?php echo $start_time . " to " . $end_time ?></p>    
-        </div>
     <?php endwhile; wp_reset_postdata(); ?>
     </ul>
-
 <?php }
 
 ?>
