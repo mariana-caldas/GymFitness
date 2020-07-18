79feb000a6fcbe080cf3102546d7715b93a44430
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-07-18 15:46:49 -0700

    adding single page style for classes

--- themes/gymfitness/single.php
+++ themes/gymfitness/single.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
-<?php while (have_posts()) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-<?php endwhile; ?>
+<main class="container page with-sidebar">
+    <div class="main-content">
+        <?php while (have_posts()) : the_post(); ?>
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    </div>
+</main>
+<?php get_footer(); ?>
--- themes/gymfitness/template-parts/page-loop.php
+++ themes/gymfitness/template-parts/page-loop.php
@@ -4,6 +4,16 @@
     if(has_post_thumbnail()):
         the_post_thumbnail('blog');
     endif;
-?>
+
+    //Check the current custom post type
+    if(get_post_type() === 'gymfitness-classes'):
+
+        $start_time = get_field('start_time');
+        $end_time = get_field('end_time');
+    ?>
+        <p class="content-post-class"><?php echo the_field('class_days') . " - " . $start_time . " to " . $end_time ?></p>
+    <?php
+    endif;
+    ?>
 <?php the_content(); ?>
 <?php endwhile; ?>
