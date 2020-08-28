386e80dc6f22bb2a1ea355f549f5f4fe23bd7be5
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-08-28 16:21:16 -0700

    adding a background to the front-page, creating variables to the text-shadows

--- themes/gymfitness/front-page.php
+++ themes/gymfitness/front-page.php
@@ -1,3 +1,13 @@
 <?php get_header('front-page'); ?>
+<?php 
+	$image = get_field('hero_image');
+    if( $image ): 
+?>
+	<style type="text/css">
+		body.front-page .site-header {
+			background-image: linear-gradient( rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(<?php echo $image['url']; ?>);
+		}
+	</style>
+<?php endif; ?>
 
 <?php get_footer(); ?>
