0997dbcf6e27caeabd4adb9b67910b4163b864b6
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-08-29 15:39:37 -0700

    adding gym areas to the front page

--- themes/gymfitness/front-page.php
+++ themes/gymfitness/front-page.php
@@ -1,18 +1,64 @@
 <?php get_header('front-page'); ?>
-<?php 
-	$image = get_field('hero_image');
-    if( $image ): 
-?>
-	<style type="text/css">
-		body.front-page .site-header {
-			background-image: linear-gradient( rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(<?php echo $image['url']; ?>);
-		}
-	</style>
-<?php endif; ?>
 <?php while(have_posts()) : the_post(); ?>
+    <?php 
+        $image = get_field('hero_image');
+        if( $image ): 
+    ?>
+        <style type="text/css">
+            body.front-page .site-header {
+                background-image: linear-gradient( rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(<?php echo $image['url']; ?>);
+            }
+        </style>
+    <?php endif; ?>
+
     <section class="container section text-center welcome-section">
-        <h2 class="text-primary"><?php the_field('welcome_title')  ?></h2>
-        <p><?php the_field('welcome_content')  ?></p>
+        <h2 class="text-primary"><?php the_field('welcome_title');  ?></h2>
+        <p><?php the_field('welcome_content');  ?></p>
+    </section>
+    <section class="section-areas">
+        <ul class="areas-container">
+            <li class="area">
+                <?php 
+                    $area1 = get_field('area_1');
+                    $image = wp_get_attachment_image_src($area1['area_image'], 'mediumSize')[0];
+                    // var_dump($image);
+                
+                ?>
+                 <img src="<?php echo $image ?>" />
+                 <p><?php echo $area1['area_name'] ?></p>
+            </li>
+            <li class="area">
+                <?php 
+                    $area2 = get_field('area_2');
+                    $image = wp_get_attachment_image_src($area2['area_image'], 'mediumSize')[0];
+                    // var_dump($image);
+                
+                ?>
+                 <img src="<?php echo $image ?>" />
+                 <p><?php echo $area2['area_name'] ?></p>
+            </li>
+            <li class="area">
+                <?php 
+                    $area3 = get_field('area_3');
+                    $image = wp_get_attachment_image_src($area3['area_image'], 'mediumSize')[0];
+                    // var_dump($image);
+                
+                ?>
+                 <img src="<?php echo $image ?>" />
+                 <p><?php echo $area3['area_name'] ?></p>
+            </li>
+            <li class="area">
+                <?php 
+                    $area4 = get_field('area_4');
+                    $image = wp_get_attachment_image_src($area4['area_image'], 'mediumSize')[0];
+                    // var_dump($image);
+                
+                ?>
+                 <img src="<?php echo $image ?>" />
+                 <p><?php echo $area4['area_name'] ?></p>
+            </li>
+        </ul>
+
     </section>
 
 <?php endwhile; ?>
