ae23d194668308c50a9a4bcacdb37fd86e28557e
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-09-27 20:51:38 -0700

    making the nav manu fixed when scrolling even when the page is reloaded

--- themes/gymfitness/js/main.js
+++ themes/gymfitness/js/main.js
@@ -10,6 +10,28 @@
             controls: false,
             mode: 'fade'
         });
+
+        //When page is ready, a fixed menu is shown when scrolling even if the page is refreshed
+        const headerScroll = document.querySelector('.navigation-bar');
+        const recBounding = headerScroll.getBoundingClientRect();
+        const topDistance = Math.round(Math.abs(recBounding.top));
+        fixedMenu(topDistance);        
     });
 
 })(jQuery);
+
+
+window.onscroll = () => {
+    let scroll = Math.round(window.scrollY);
+    fixedMenu(scroll);
+}
+
+//Adds a fixed menu on top
+function fixedMenu(scrollEffect){
+    const headerScroll = document.querySelector('.navigation-bar');
+    if(scrollEffect > 200){
+        headerScroll.classList.add('navFixedTop');
+    } else {
+        headerScroll.classList.remove('navFixedTop');
+    }
+}
