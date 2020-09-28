b45338bab3c1fc5ce96879fd3abed68e29021058
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-09-28 12:19:32 -0700

    making the project the page iOS and Android compatible

--- themes/gymfitness/header.php
+++ themes/gymfitness/header.php
@@ -4,9 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php bloginfo('description'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <!--Make the page iOS and Android compatible -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
+    <meta name="theme-color" content="#ff5b00">
+    <meta name="mobile-web-app-capable" content="yes">
+    <!--------------------------------------------->
     <?php wp_head() ?>
 </head>
 
